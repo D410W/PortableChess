9090f0a0dedd8260f20645704cc69491c88a422e
thalesWindows <thalesjdc61@gmail.com>
2026-03-20 20:02:07 -0300

    initial android app setup complete

---
+++ app/src/main/java/com/d410w/portablechess/MainActivity.java
@@ -0,0 +1,12 @@
+package com.d410w.portablechess;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_main); // Uncomment this later when you make a UI layout
+    }
+}
